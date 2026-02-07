ebbc64bdeda63e90615ceeb0f160d37e081c1487
Bernardo Santiago <bernardosantiago44@gmail.com>
2026-02-07 11:11:00 -0600

    Initial commit

---
+++ MapcelRepositorioArticulos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MapcelRepositorioArticulos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
